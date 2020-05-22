e0fb7fda89adee123083fb430e946b86e1662bc1
root <root@localhost.localdomain>
2020-05-22 19:40:11 -0300

    Crontab Servidor Laboratório

--- crowd/appso.py
+++ crowd/appso.py
@@ -260,10 +260,33 @@ def chart3():
 
 @app.route("/simple_chart4")
 def draw():
-    x = [2, 4, 6]
-    y = [1, 3, 5]
-    plt.plot(x, y)
-    return plt.show()
+    import datetime
+    import StringIO
+    import random
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+    import make_response 
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = StringIO.StringIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
 
 #	return render_template('plot.html', url='static/images/plot.png')
 
