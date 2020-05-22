577b505c0b1f19e1e8c9a42147af976c849024e1
root <root@localhost.localdomain>
2020-05-22 19:00:11 -0300

    Crontab Servidor Laboratório

--- crowd/appso.py
+++ crowd/appso.py
@@ -26,9 +26,6 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
 
 
 app = Flask(__name__)
