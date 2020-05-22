b2f5ecf16f7eccf5305219b6ba7da778afdf236e
root <root@localhost.localdomain>
2020-05-22 18:35:11 -0300

    Crontab Servidor Laboratório

--- crowd/appso.py
+++ crowd/appso.py
@@ -253,9 +253,24 @@ def chart3():
 
 @app.route("/simple_chart4")
 def draw():
- 	lnprice=np.log(price)
- 	plt.plot(lnprice)
- 	return render_template('untitled1.html', name = plt.show())
+	left = [1, 2, 3, 4, 5]
+    # heights of bars
+	height = [10, 24, 36, 40, 5]
+    # labels for bars
+	tick_label = ['one', 'two', 'three', 'four', 'five']
+    # plotting a bar chart
+	plt.bar(left, height, tick_label=tick_label, width=0.8, color=['red', 'green'])
+
+    # naming the y-axis
+	plt.ylabel('y - axis')
+    # naming the x-axis
+	plt.xlabel('x - axis')
+    # plot title
+	plt.title('My bar chart!')
+
+	plt.savefig('static/images/plot.png')
+
+	return render_template('plot.html', url='/static/images/plot.png')
 
 @app.route('/teste')
 def teste():
