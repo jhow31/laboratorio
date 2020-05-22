0f2e5faae6e71535015af64dc1a19e829c399be6
root <root@localhost.localdomain>
2020-05-22 19:35:11 -0300

    Crontab Servidor Laboratório

--- crowd/appso.py
+++ crowd/appso.py
@@ -260,24 +260,10 @@ def chart3():
 
 @app.route("/simple_chart4")
 def draw():
-	left = [1, 2, 3, 4, 5]
-    # heights of bars
-	height = [10, 24, 36, 40, 5]
-    # labels for bars
-	tick_label = ['one', 'two', 'three', 'four', 'five']
-    # plotting a bar chart
-	plt.bar(left, height, tick_label=tick_label, width=0.8, color=['red', 'green'])
-
-    # naming the y-axis
-	plt.ylabel('y - axis')
-    # naming the x-axis
-	plt.xlabel('x - axis')
-    # plot title
-	plt.title('My bar chart!')
-
-	plt.savefig('static/images/plot.png')
-
-	return plt.show()
+    x = [2, 4, 6]
+    y = [1, 3, 5]
+    plt.plot(x, y)
+    return plt.show()
 
 #	return render_template('plot.html', url='static/images/plot.png')
 
