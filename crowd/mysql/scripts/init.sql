ce2cbd39f985acbfe09533a651c8f2226b39ad41
root <root@localhost.localdomain>
2020-05-23 19:50:16 -0300

    Crontab Servidor Laboratório

--- crowd/mysql/scripts/init.sql
+++ crowd/mysql/scripts/init.sql
@@ -1,28 +1,9 @@
 GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'root';
 create database crowd;
 use crowd;
-create table venda(
-   id_venda INT NOT NULL AUTO_INCREMENT,
-   valor INT(250),
-   quantidade INT(250),
-   categoria VARCHAR(100) NOT NULL,
-   produto VARCHAR(40) NOT NULL,
-   date_now datetime NOT NULL,  
-   PRIMARY KEY (id_venda)
-);
-create table produtos(
-   id_produto INT NOT NULL AUTO_INCREMENT,
-   produto VARCHAR(250),
-   valor INT(250),
-   descricao VARCHAR(100),
-   quantidade INT(250),
-   categoria VARCHAR(100) NOT NULL,
-   id_user int(250),
-   PRIMARY KEY (id_produto)
-);
 create table estoque(
    id_estoque INT NOT NULL AUTO_INCREMENT,
-   valor INT(250),
+   valor decimal(15,2),
    quantidade INT(250),
    categoria VARCHAR(100) NOT NULL,
    produto VARCHAR(40) NOT NULL,
@@ -30,12 +11,35 @@ create table estoque(
    user_is INT(250) NOT NULL,
    PRIMARY KEY (id_estoque)
 );
+
+
+create table venda(
+   id_venda INT NOT NULL AUTO_INCREMENT,
+   valor decimal(15,2),
+   quantidade INT(250),
+   categoria VARCHAR(100) NOT NULL,
+   produto VARCHAR(40) NOT NULL,
+   date_now datetime NOT NULL,  
+   PRIMARY KEY (id_venda)
+);
+
+create table produtos(
+   id_produto INT NOT NULL AUTO_INCREMENT,
+   produto VARCHAR(250),
+   valor decimal(15,2),
+   descricao VARCHAR(100),
+   quantidade INT(250),
+   categoria VARCHAR(100) NOT NULL,
+   id_user int(250),
+   PRIMARY KEY (id_produto)
+);
+
 create table user(
    id_user INT NOT NULL AUTO_INCREMENT,
    nome VARCHAR(250),
    sobrenome VARCHAR(100),
-   cpf BIGINT(13),
-   telefone BIGINT(250),
+   cpf INT(13),
+   telefone INT(250),
    email VARCHAR(250),
    cep VARCHAR(250),
    password VARCHAR(100) NOT NULL,
