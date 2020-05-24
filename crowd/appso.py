16f0d27e041fbc30cc0da8f9ab5c4f7d3687234e
root <root@localhost.localdomain>
2020-05-24 14:45:12 -0300

    Crontab Servidor Laboratório

--- crowd/appso.py
+++ crowd/appso.py
@@ -223,7 +223,7 @@ def chart():
 		chart.add(row[0], [row[1]])
 # 		print(data)
 		graph_produtos = chart.render_data_uri()
-        cur.execute("select produto, valor as teste from venda group by produto;")
+        cur.execute("select produto, sum(valor) as teste from venda group by produto")
         chart = pygal.Bar()
         data3 = cur.fetchall()
         for row in data3:
